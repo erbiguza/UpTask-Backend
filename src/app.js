a89be1d609dfb27bd9758223fc410724e6a1e5aa
Erbi Guza <erbiguza@gmail.com>
2026-03-01 11:36:07 +0100

    added cors to connect to react frontend

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import authRouter from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 export const startApp = () => {
     const app = express();
 
     app.use(express.json());
     app.use(cookieParser());
+    app.use(
+        cors({
+            origin: `${process.env.REACT_FRONTEND_URL}`,
+            credentials: true,
+        }),
+    );
 
-    app.use("/api/v1/auth", authRouter);
+    app.use(`${process.env.API}/auth`, authRouter);
 
     return app;
 };
